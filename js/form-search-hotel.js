dabec7bc2b4a195d76ac18e4bb9801b054ee0332
MariyaKustova <masha.kustova-guselnikova@yandex.ru>
2020-02-24 16:15:54 +0500

    Оптимизировала JS с помощью function

--- js/form-search-hotel.js
+++ js/form-search-hotel.js
@@ -1,100 +1,40 @@
-var closeOpenForm=document.querySelector(".button-brown");
-var formReservation=document.querySelector(".form-reservation");
+var closeOpenFormButton = document.querySelector(".button-brown");
+var formReservation = document.querySelector(".form-reservation");
 
-var searchResults=formReservation.querySelector(".search-options");
+var searchResultsButton = formReservation.querySelector(".search-options");
 
-var arrivalDate=formReservation.querySelector("[name=arrival-date]");
-var dateDeparture=formReservation.querySelector("[name=date-departure]");
-var adultsGuests=formReservation.querySelector("[name=number-adults-guests]");
-var childrenGuests=formReservation.querySelector("[name=number-children-guests]");
-var buttonAddGuest=formReservation.querySelector("[name=add-guests]");
-var buttonSubtractGuest=formReservation.querySelector("[name=subtract-guests]");
+var arrivalDate = formReservation.querySelector("[name=arrival-date]");
+var dateDeparture = formReservation.querySelector("[name=date-departure]");
+var adultsGuests = formReservation.querySelector("[name=number-adults-guests]");
+var childrenGuests = formReservation.querySelector(
+  "[name=number-children-guests]"
+);
+var buttonGuest = formReservation.querySelector(".button-guests");
 
 
-closeOpenForm.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    if(formReservation.classList.contains('close-form')) {
-        formReservation.classList.remove("close-form");
-    }else {
-        formReservation.classList.add("close-form");
-    }    
+closeOpenFormButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  formReservation.classList.toggle("close-form");
 });
 
-closeOpenForm.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 13) {
+closeOpenFormButton.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 13) {
     evt.preventDefault();
-    if(formReservation.classList.contains('close-form')) {
-        formReservation.classList.remove("close-form");
-    }else {
-        formReservation.classList.add("close-form");
-    }    
+    formReservation.classList.toggle("close-form");
 }});
 
-closeOpenForm.addEventListener("keydown", function(evt) {
+function closeFormReservation(evt) {
     if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.remove("close-form");
-    }  
-});
-
-arrivalDate.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-dateDeparture.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-adultsGuests.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-childrenGuests.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-buttonAddGuest.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-buttonSubtractGuest.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-searchResults.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-    evt.preventDefault();
-    formReservation.classList.add("close-form");
-    closeOpenForm.focus();
-    }  
-});
-
-searchResults.addEventListener("submit", function(evt){
-    if(!arrivalDate.value||!dateDeparture.value) {
         evt.preventDefault();
-    }
+        formReservation.classList.add("close-form");
+        closeOpenFormButton.focus();
+      }
+}
+
+formReservation.addEventListener("keydown", closeFormReservation);
+
+formReservation.addEventListener("submit", function(evt) {
+  if (!arrivalDate.value || !dateDeparture.value) {
+    evt.preventDefault();
+  }
 });
